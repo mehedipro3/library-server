42c4a6da1d3b3a9b55c1811c1ce1e35394591b57
mehedipro3 <mehediratul37@gmail.com>
2025-04-19 00:45:06 +0600

    BorrowedBooks added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 3000;
 
 // Middleware
@@ -29,22 +29,88 @@ async function run() {
 
     // Declare collections
     const booksCollection = client.db('book-category').collection('books');
-   
+    const borrowCollection = client.db('book-category').collection('borrowedBooks')
 
-    // GET books with optional category filter
-    app.get('/books',  async(req, res)=> {
+    // GET books
+    app.get('/books', async (req, res) => {
       const category = req.query.category;
       let query = {};
-      if(category){
-        query = { category : category}
+      if (category) {
+        query = { category: category }
       }
       const cursor = booksCollection.find(query);
       const result = await cursor.toArray();
       res.send(result);
     })
 
-    
+    //Post books
 
+    app.post('/books', async (req, res) => {
+      const newBooks = req.body;
+      const result = await booksCollection.insertOne(newBooks);
+      res.send(result);
+    })
+
+    app.get('/books/:id', async (req, res) => {
+      const id = req.params;
+      const result = await booksCollection.findOne({ _id: new ObjectId(id) });
+      res.send(result);
+    })
+
+    app.put('/books/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) }
+      const updatedBook = req.body;
+      const book = {
+        $set: {
+          name: updatedBook.name,
+          image: updatedBook.image,
+          category: updatedBook.category,
+          quantity: updatedBook.quantity,
+          rating: updatedBook.rating,
+          author: updatedBook.author,
+          description: updatedBook.description,
+        }
+      }
+      const result = await booksCollection.updateOne(filter, book)
+      res.send(result)
+    })
+
+
+    app.get('/bookBorrowed', async (req, res) => {
+      const email = req.query.email;
+      let query = {};
+      if (email) {
+        query = { userEmail: email }
+      }
+
+      // if (req.user.email !== req.query.email  ) {
+      //   return req.status(403).send({ massage: "Forbidden Access" });
+      // }
+
+      const cursor = borrowCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    app.post('/borrow/:id', async (req, res) => {
+      const id = req.params.id;
+      const borrowBookDetails = req.body;
+
+      await booksCollection.updateMany(
+        { quantity: { $type: "string" } },
+        [
+          { $set: { quantity: { $toInt: "$quantity" } } }
+        ]
+      );
+
+      await booksCollection.updateOne(
+        { _id: new ObjectId(id) },
+        { $inc: { quantity: -1 } }
+      );
+      const result = await borrowCollection.insertOne(borrowBookDetails);
+      res.send(result)
+    })
 
     // Test route
     app.get('/', (req, res) => {
